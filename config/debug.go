1e969c9b6ee8fb11acd1449b15cea8297834a51b
nicoh <harbig.nico@protonmail.com>
2025-09-21 21:41:21 +0200

    feat: Make debug flag available from environment

--- config/debug.go
+++ config/debug.go
@@ -1,10 +1,19 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var Debug bool
 
 func Init() {
 	flag.BoolVar(&Debug, "debug", false, "enable debug mode")
 	flag.Parse()
+	if !Debug {
+		val, ok := os.LookupEnv("DEBUG")
+		if ok {
+			Debug = val == "true"
+		}
+	}
 }
