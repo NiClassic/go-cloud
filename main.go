41c0bab5237e8dc156b1a0fe4337c46f9c3322f1
nicoh <harbig.nico@protonmail.com>
2025-09-21 21:08:21 +0200

    chore: Move database initialization into own file

--- main.go
+++ main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"database/sql"
-	"errors"
 	"github.com/NiClassic/go-cloud/config"
+	"github.com/NiClassic/go-cloud/internal/db"
 	"github.com/NiClassic/go-cloud/internal/handler"
 	"github.com/NiClassic/go-cloud/internal/logger"
 	"github.com/NiClassic/go-cloud/internal/service"
 	"github.com/NiClassic/go-cloud/internal/storage"
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/sqlite"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/joho/godotenv"
 	_ "modernc.org/sqlite"
@@ -26,43 +24,32 @@ func main() {
 	if err != nil {
 		logger.Fatal("could not load .env file: %v", err)
 	}
-	db, err := sql.Open("sqlite", os.Getenv("DB_FILE"))
-	if err != nil {
-		logger.Fatal("could not open database: %v", err)
-	}
-	if err = db.Ping(); err != nil {
-		logger.Fatal("could not ping database: %v", err)
-	}
-	logger.Info("successfully connected to database")
 
-	driver, err := sqlite.WithInstance(db, &sqlite.Config{})
+	dbConn, err := db.New()
 	if err != nil {
-		logger.Fatal("could not open database for migrations: %v", err)
+		logger.Fatal("could not connect to db: %v", err)
 	}
-	m, err := migrate.NewWithDatabaseInstance("file://./db/migrations", "sqlite", driver)
-	if err != nil {
-		logger.Fatal("could not initialize migrations: %v", err)
+
+	if err := db.Migrate(dbConn); err != nil {
+		logger.Fatal("could not apply migrations: %v", err)
 	}
-	if err = m.Up(); err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		logger.Fatal("could not run migrations: %v", err)
-	}
+
 	defer func(db *sql.DB) {
-		err := db.Close()
-		if err != nil {
+		if err = db.Close(); err != nil {
 			logger.Fatal("could not close database: %v", err)
 		}
-	}(db)
+	}(dbConn)
 
-	storage := storage.NewStorage(os.Getenv("DATA_ROOT"))
+	st := storage.NewStorage(os.Getenv("DATA_ROOT"))
 
-	services := service.InitServices(db, storage)
+	services := service.InitServices(dbConn, st)
 
 	tmpl, err := handler.ParseTemplates()
 	if err != nil {
 		logger.Fatal("could not parse templates: %v", err)
 	}
 
-	mux := handler.New(services, storage, tmpl)
+	mux := handler.New(services, st, tmpl)
 
 	logger.Info("listening on :8080 (Debug Mode=%v)", config.Debug)
 	if err = http.ListenAndServe(":8080", mux); err != nil {
